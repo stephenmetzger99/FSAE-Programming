c66e19d53e6df07e04311d2cfeb39c40111cdec2
NicoMitchell <namitc02@louisville.edu>
2018-06-13 01:05:35 -0400

    groundwork for canbus

--- canbus/canbus.ino/canbus.ino.ino
+++ canbus/canbus.ino/canbus.ino.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Canbus.h"
 
+void setup() {
+  Serial.begin(9600); // For debug use
+  while(!Canbus.init(CANSPEED_500))  //Initialise MCP2515 CAN controller at the specified speed
+  {
+    if (Canbus.init(CANSPEED_500)) {
+      break;
+    }
+  }
 }
 
+
+char buffer[64];
+char* rpm_rawdata;
+uint16_t rpm;
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
